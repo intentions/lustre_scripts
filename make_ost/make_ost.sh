475f007f78b2d6c5791eab56288edd1011bd10b5
Kurt Strosahl <strosahl@jlab.org>
2015-03-18 16:17:05 -0400

    added ignore files 	new file:   .gitignore 	this ignore file ignores .conf files to prevent propogation of 	site specific configurations 	new file:   make_ost/.gitignore 	modified to get variable names from ost.conf 	modified:   make_ost/make_ost.sh 	template for the ost.conf file 	new file:   make_ost/ost.conf_template

--- make_ost/make_ost.sh
+++ make_ost/make_ost.sh
@@ -4,25 +4,39 @@
 #written by: Kurt J. Strosahl (strosahl@jlab.org)
 #Written: 18MAR15
 #
-#
-# To create a new ost increment the index, update the zpool name, and ensure that the VDEVS list points to the data disks on the system
-# the vdev aliases are found in /dev/disk/by-vdev, and it is built by the following
-# create /etc/zfs/vdev_id.conf and populate it by looking at the /dev/disk/by-path/ directory.
-# Create an alias for all disks, then comment out aliases that point to non-data disks
-# after the alias file is fully populated use the command udevadmn trigger to populate the /dev/disk/by-vdev directory
-# then verify that only data disks are listed by using fdisk -l /dev/disk/by-vdev/* (this assumes that the data disks are all the same size,
-# and that that size differs from other disks on the sytem).
-# 
-# After that is done then this script can be run, generating the ldev.conf file used by luster as /tmp/new.ldev.conf
-# assuming no errors are encountered then /tmp/new.ldev.conf can be moved to /etc/ldev.conf
-# then lustre can be started using /etc/init.d/lustre start
-#
-# note that due to pathing issues the service command cannot be used to start lustre.
+
+HELP="
+ To create a new ost increment the index, update the zpool name, and ensure that the VDEVS list points to the data disks on the system
+ the vdev aliases are found in /dev/disk/by-vdev, and it is built by the following
+ create /etc/zfs/vdev_id.conf and populate it by looking at the /dev/disk/by-path/ directory.
+ Create an alias for all disks, then comment out aliases that point to non-data disks
+ after the alias file is fully populated use the command udevadmn trigger to populate the /dev/disk/by-vdev directory
+ then verify that only data disks are listed by using fdisk -l /dev/disk/by-vdev/* (this assumes that the data disks are all the same size,
+ and that that size differs from other disks on the sytem).
+ 
+ After that is done then this script can be run, generating the ldev.conf file used by luster as /tmp/new.ldev.conf
+ assuming no errors are encountered then /tmp/new.ldev.conf can be moved to /etc/ldev.conf
+ then lustre can be started using /etc/init.d/lustre start
+
+ note that due to pathing issues the service command cannot be used to start lustre.
+
+ use syntax:
+"
+
+if [[ $1 == "-h" || $1 =="--help" ]]; then
+	echo $HELP
+fi
+
+TIMESTAMP=`date +%d%^b%y`
+LOGNAME=${LOG_NAME:-"ost_build"}-$TIMESTAMP".log"
+
+#sourcing config file
+[ -f ost.conf ] && . ost.conf
 
 #variable list
 
 #index, sould be one higher then index of last installed ost
-INDEX="1"
+INDEX=$OST_INDEX
 
 #sets hex index for use in ldev.conf, then pads it with a zero if it is a single digit
 HEXINDEX=`echo "obase=16; $INDEX" | bc`
@@ -36,17 +50,19 @@ ZPOOLNAME="lustre-ost$INDEX/ost$INDEX"
 #list of disk aliases from /etc/zfs/vdev_id.conf
 #VDEVS="disk2 disk3 disk4 disk5 disk6 disk7 disk8 disk9 disk10 disk11 disk12 disk13 disk14 disk15 disk16 disk17 disk18 disk19 disk20 disk21 disk22 disk23 disk24 disk25 disk26 disk27 disk28 disk29 disk30 disk31"
 #test of easier way...
-VDEVS="/dev/disk/by-vdev/*"
+VDEVS=$VDEV_LIST
 
 #File system name, static
-FSNAME="lustre2"
+FSNAME=${FILESYSTEM_NAME:-"lustre2"}
 
 #head mds head nodes
-MGSNODE="172.17.4.125@o2ib"
-FAILNODE="172.17.4.126@o2ib"
+MGSNODE=$MGS_NODE
+FAILNODE=$FAILOVER_NODE
+#MGSNODE="172.17.4.125@o2ib"
+#FAILNODE="172.17.4.126@o2ib"
 
 #raid level
-RAIDLVL="raidz2"
+RAIDLVL=${RAID_LEVEL:-"raidz2"}
 
 #node name
 NODENAME=`uname -n`
@@ -63,3 +79,5 @@ EOF
 
 #build the file system
 mkfs.lustre --fsname=$FSNAME --ost --backfstype=zfs --index=$INDEX --mgsnode=$MGSNODE --failnode=$FAILNODE $ZPOOLNAME $RAIDLVL $VDEVS
+
+
