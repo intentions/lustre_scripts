e95bbe07138ed145c2378bb46e616e535e8137dc
Kurt Strosahl <strosahl@jlab.org>
2015-03-18 17:03:40 -0400

    converted more variables to use the .conf, added detailed 	logging 	modified:   make_ost.sh 	updated with new variables from make_ost.sh 	modified:   ost.conf_template

--- make_ost/make_ost.sh
+++ make_ost/make_ost.sh
@@ -27,16 +27,23 @@ if [[ $1 == "-h" || $1 =="--help" ]]; then
 	echo $HELP
 fi
 
-TIMESTAMP=`date +%d%^b%y`
-LOGNAME=${LOG_NAME:-"ost_build"}-$TIMESTAMP".log"
-
 #sourcing config file
 [ -f ost.conf ] && . ost.conf
 
+#setting up log file
+TIMESTAMP=`date +%d%^b%y`
+LOGNAME=${LOG_NAME:-"/tmp/ost_build"}-$TIMESTAMP".log"
+
 #variable list
 
+#File system name, static
+FSNAME=${FILESYSTEM_NAME:-""}
+
 #index, sould be one higher then index of last installed ost
-INDEX=$OST_INDEX
+INDEX=${OST_INDEX:-""}
+
+#setting reformat flag, used only to redo the process
+REFORMAT=${SET_REFORMAT:-""}
 
 #sets hex index for use in ldev.conf, then pads it with a zero if it is a single digit
 HEXINDEX=`echo "obase=16; $INDEX" | bc`
@@ -46,29 +53,61 @@ fi
 
 #pool name, based on index
 ZPOOLNAME="lustre-ost$INDEX/ost$INDEX"
+MOUNTDIR=${MOUNT_DIRECTORY:-"/ost$INDEX"}
 
 #list of disk aliases from /etc/zfs/vdev_id.conf
 #VDEVS="disk2 disk3 disk4 disk5 disk6 disk7 disk8 disk9 disk10 disk11 disk12 disk13 disk14 disk15 disk16 disk17 disk18 disk19 disk20 disk21 disk22 disk23 disk24 disk25 disk26 disk27 disk28 disk29 disk30 disk31"
 #test of easier way...
-VDEVS=$VDEV_LIST
-
-#File system name, static
-FSNAME=${FILESYSTEM_NAME:-"lustre2"}
+VDEVS=${VDEV_LIST:-""}
 
 #head mds head nodes
-MGSNODE=$MGS_NODE
-FAILNODE=$FAILOVER_NODE
-#MGSNODE="172.17.4.125@o2ib"
-#FAILNODE="172.17.4.126@o2ib"
+MGSNODE=${MGS_NODE:-""}
+FAILNODE=${FAILOVER_NODE:-""}
+
+#backend file system
+BACKENDFS=${BACKEND_FILESYSTEM:-"zfs"}
 
 #raid level
-RAIDLVL=${RAID_LEVEL:-"raidz2"}
+RAIDLVL=${RAID_LEVEL:-""}
 
 #node name
 NODENAME=`uname -n`
 
+#new ldev.conf file
+LDEVCONF=${LDEV_CONF:-"/tmp/new.ldev.conf"}
+
+#lustre configuration file
+LUSTRECONF=${LUSTRE_CONFIG:-"/etc/sysconfig/lustre"}
+
+#logfile initialization
+cat > LOGNAME <<EOF
+ost creation log
+DATE: $TIMESTAMP
+Filesystem name used: $FSNAME
+Reformat flag: $REFORMAT
+Backend file system: $BACKENDFS
+Index provided: $INDEX
+MGS node: $MGSNODE
+Failover node: $FAILNODE
+ZPool name: $ZPOOL
+Raid level: $RAIDLVL
+list of disk for the zpool: $VDEVS
+ost mount directory: $MOUNTDIR
+new ldev file: $LDEVCONF
+EOF
+
+#create local directory structure
+if [ ! -d "$MOUNTDIR" ]
+then
+	mkdir $MOUNTDIR
+	echo "appending mount directory $MOUNTDIR to $LUSTRECONF"
+	echo "LOCAL_MOUNT_DIR=$MOUNTDIR" >> $LUSTRECONF
+else
+	echo "$MOUTDIR already exists, skipping directory creation and appending to $LUSTRECONF"
+fi
+
 #create the entries in /etc/ldev.conf
-cat > /tmp/new.ldev.conf <<EOF
+cat > $LDEVCONF <<EOF
 # example /etc/ldev.conf
 #
 #local  foreign/-  label       [md|zfs:]device-path   [journal-path]/- [raidtab]
@@ -78,6 +117,6 @@ $NODENAME - $FSNAME-OST00$HEXINDEX zfs:$ZPOOLNAME
 EOF
 
 #build the file system
-mkfs.lustre --fsname=$FSNAME --ost --backfstype=zfs --index=$INDEX --mgsnode=$MGSNODE --failnode=$FAILNODE $ZPOOLNAME $RAIDLVL $VDEVS
+mkfs.lustre --fsname=$FSNAME $REFORMAT --ost --backfstype=$BACKENDFS --index=$INDEX --mgsnode=$MGSNODE --failnode=$FAILNODE $ZPOOLNAME $RAIDLVL $VDEVS 1>> $LOGNAME 2>&1
 
 
